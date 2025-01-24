3d2dc965049cb298bb3ea8debeff64d2f7fe5e7a
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-01-24 16:40:09 -0300

    Update While

--- 6-While.py
+++ 6-While.py
@@ -166,3 +166,206 @@ print("Média dos número digitados: {}\n"
       "Menor valor digitado: {}\n"
       "Maior valor digitado: {}".format(media,menor,maior))
 print()
+
+
+#---------------------------------------------
+print("DESAFIO - TABUADA DE VÁRIOS NÚMEROS")
+numero=0
+while numero>=0:
+    numero = int(input("Tabuada de qual número? (-1 para sair)\n--> "))
+    if numero>=0:
+        for cont in range(1,11):
+            print("{} x {} = {}".format(numero,cont,(cont*numero)))
+        print()
+print()
+
+
+#---------------------------------------------
+print("DESAFIO - PAR OU ÍMPAR")
+import time
+import random
+vitoria=True
+contador_vitoria=soma=0
+
+while vitoria==True:#ENQUANTO O JOGADOR GANHAR
+    op_jogador=input("Você quer jogar par ou ímpar? ")
+    op_jogador=op_jogador.upper()
+    #LAÇO PARA GARANTIR UM ENTRADA PAR OU IMPAR
+    while op_jogador not in ['PAR','P','IMPAR','ÍMPAR','I']:
+        print("Entrada inválida! Tente novamente.")
+        print()
+        op_jogador=input("Você quer jogar par ou ímpar? ")
+        op_jogador=op_jogador.upper()
+
+    #CASO O JOGADOR JOGUE PAR
+    if op_jogador=="PAR" or op_jogador=="P":
+        op_computador="IMPAR"
+        op_jogador="PAR"
+        #LAÇO PARA GARANTIR UMA ENTRADA INTEIRA PAR
+        numero_pessoa = int(input("Digite um número inteiro par: "))
+        while numero_pessoa%2!=0:#enquanto a pessoa não digitar um número par
+            numero_pessoa = int(input("Você não digitou um número par.\nDigite novamente: "))
+
+    #CASO O JOGADOR JOGUE ÍMPAR
+    if op_jogador=="IMPAR" or op_jogador=="ÍMPAR" or op_jogador=="I":
+        op_computador="PAR"
+        op_jogador="IMPAR"
+        #LAÇO PARA GARANTIR UMA ENTRADA INTEIRA ÍMPAR
+        numero_pessoa = int(input("Digite um número inteiro ímpar: "))
+        while numero_pessoa % 2 == 0:  # enquanto a pessoa não digitar um número ímpar
+            numero_pessoa = int(input("Você não digitou um número ímpar.\nDigite novamente: "))
+
+
+
+    print("Computador jogou.... ",end="")
+    time.sleep(1)
+    #COMPUTADOR GERA UM NÚMERO INTEIRO ALEATÓRIO ENTRE 0 E 1000
+    numero_computador=int(random.randint(0,1000))
+    print(numero_computador)
+    soma=numero_pessoa+numero_computador
+    print("Somando os valores temos -> {}".format(soma),end=" ")
+
+    #SE O JOGADOR TIVER ESCOLHIDO PAR
+    if op_jogador=="PAR":
+        if soma%2==0:#soma par
+            contador_vitoria += 1#INCREMENTO CONTADOR VITÓRIA
+            print("Deu par! Você ganhou!")
+        elif soma%2!=0:#soma impar
+            print("Deu ímpar! Computador ganhou!")
+            vitoria = False#CONDIÇÃO PARA SAÍDA DO LAÇO
+
+    #SE O JOGADOR TIVER ESCOLHIDO ÍMPAR
+    elif op_jogador=="IMPAR":
+        if soma%2==0:#soma par
+            print("Deu par! Computador ganhou!")
+            vitoria = False
+        elif soma%2!=0:#soma impar
+            contador_vitoria += 1
+            print("Deu ímpar! Você ganhou!")
+
+print("\nFim de jogo!\nVocê ganhou {} rodadas".format(contador_vitoria))
+print()
+
+
+#---------------------------------------------
+print("DESAFIO - IDADE E SEXO DE VÁRIAS PESSOAS")
+continuar=True
+cont=1
+maior_18=homens=mulheres_menos20=0
+#ENQUANTO A PESSOA ESCOLHER CONTINUAR
+while continuar==True:
+    #ENTRADA DA IDADE
+    idade=int(input("Idade da pessoa {}: ".format(cont)))
+
+    #ENTRADA DO SEXO COM VERIFICAÇÃO
+    sexo=input("Sexo da pessoa {} [F/M]: ".format(cont))
+    sexo=sexo.upper()
+    #SE HOMEM
+    while sexo not in ["M","MASCULINO","HOMEM","F","FEMININO","MULHER"]:
+        print("Entrada inválida. Digite novamente!")
+        sexo = input("Sexo da pessoa {} [F/M]: ".format(cont))
+        sexo = sexo.upper()
+    if sexo in ["M", "MASCULINO", "HOMEM"]:
+        homens+=1
+
+    #SE MAIOR DE IDADE
+    if idade>18:
+        maior_18+=1
+
+    #QUANTAS MULHERES MAIS DE 20 ANOS
+    if sexo=="F" or sexo=="FEMININO" or sexo=="MULHER":
+        if idade<20:
+            mulheres_menos20+=1
+    
+    #OPÇÃO AO USUÁRIO DE CONTINUAR OU NÃO
+    op=input("Deseja continuar? [S/N]")
+    op=op.upper()
+    while op not in ["S","N","SIM","NÃO","NAO"]:
+        print("Entrada inválida. Tente novamente!")
+        op = input("Deseja continuar? [S/N]")
+        op = op.upper()
+    if op=="N" or op=="NÃO" or op=="NAO":
+        continuar=False
+    cont+=1#INCREMENTO CONTADOR DE REPETIÇÕES DO LAÇO
+
+print()
+print("Número de pessoas com mais de 18 anos: {}\n"
+      "Número de homens cadastrados: {}\n"
+      "Número de mulheres com menos de 20 anos: {}".format(maior_18,homens,mulheres_menos20))
+print()
+
+
+#---------------------------------------------
+print("DESAFIO - PREÇO E NOME DE VÁRIOS PRODUTOS")
+continuar=True
+cont=1
+total_compra=produtos_maior_que_mil=0
+#A VARIÁVEL QUE GUARDA O VALOR DO PRODUTO DE MENOR VALOR É INICIALIZADA COM
+#UM NÚMERO GRANDE, PRA QUE NA COMPARAÇÃO, ELA POSSA ASSUMIR UM NÚMERO PEQUENO
+mais_barato=1000000
+nome_mais_barato=""
+
+while continuar==True:
+    nome=input("Nome do {}º produto: ".format(cont))
+    valor=float(input("Preço do {}º produto: ".format(cont)))
+    #TOTAL DA COMPRA INCREMENTADO PELO VALOR DO ITEM
+    total_compra+=valor
+    #INCREMENTO CONTADOR DE PRODUTOS MAIS CAROS QUE 1000
+    if valor>1000:
+        produtos_maior_que_mil+=1
+    #SE FOR O PRODUTO MAIS BARATO, SALVO O NOME
+    if valor<mais_barato:
+        mais_barato=valor
+        nome_mais_barato=nome
+
+    #OPÇÃO AO USUÁRIO DE CONTINUAR OU NÃO
+    op = input("Deseja continuar? [S/N]")
+    op = op.upper()
+    while op not in ["S", "N", "SIM", "NÃO","NAO"]:
+        print("Entrada inválida. Tente novamente!")
+        op = input("Deseja continuar? [S/N]")
+        op = op.upper()
+    if op in ["N","NAO","NÃO"]:
+        continuar = False
+    cont += 1  # INCREMENTO CONTADOR DE REPETIÇÕES DO LAÇO
+
+#MOSTRANDO NA TELA OS RESULTADOS
+print("Total gasto na compra: {}\n"
+      "Quantos produtos custam mais que R$1.000,00:  {}\n"
+      "Nome do produto mais barato:  {}".format(total_compra,produtos_maior_que_mil,nome_mais_barato))
+print()
+
+
+#---------------------------------------------
+print("DESAFIO - CAIXA ELETRÔNIOCO EMITINDO CÉDULAS")
+saque=int(input("Qual o valor do saque? "))
+#INICIALIZAÇÃO DAS VARIÁVEIS
+cedulas_100=cedulas_50=cedulas_20=cedulas_10=cedulas_1=0
+
+#ENQUANTO NÃO TIVER EMITIDO TODO O VALOR DO SAQUE
+while saque!=0:
+    #SE O VALOR FOR DIVISÍVEL POR 100
+    if (saque/100)>=1:
+        #NÚMERO DE CÉDULAS DE 100 É O VALOR INTEIRO DA DIVISÃO DO SAQUE POR 100
+        cedulas_100 = int(saque / 100)
+        #VARIÁVEL SAQUE RECEBE O RESTO DA DIVISÃO POR 100
+        saque = saque % 100
+        print("Cédulas R$100: {}".format(cedulas_100))
+    #PARA AS DEMAIS CÉDULAS, DADO O NOVO VALOR DA VARIÁVEL SAQUE, SE REPETE O PROCEDIMENTO
+    elif (saque/50)>=1:
+        cedulas_50 = int(saque / 50)
+        saque = saque % 50
+        print("Cédulas R$50: {}".format(cedulas_50))
+    elif (saque/20)>=1:
+        cedulas_20 = int(saque / 20)
+        saque = saque % 20
+        print("Cédulas R$20: {}".format(cedulas_20))
+    elif (saque/10)>=1:
+        cedulas_10 = int(saque / 10)
+        saque = saque % 10
+        print("Cédulas R$10: {}".format(cedulas_10))
+    else:
+        cedulas_1 = saque
+        print("Cédulas R$1: {}".format(cedulas_1))
+        #CASO CHEGUE NAS CÉDULAS DE 1, SAI DO LAÇO
+        break
